d2e288d8988a783250f48572e9440ef24f809d43
kun <kkk@>
2025-04-07 00:41:38 +0800

    day3

--- src/leetcode/editor/cn/ID240SearchA2dMatrixIi .java
+++ src/leetcode/editor/cn/ID240SearchA2dMatrixIi .java
@@ -11,9 +11,27 @@ class ID240SearchA2dMatrixIi{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int row = 0;
+        int col = n - 1;
+
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--; // 往左
+            } else {
+                row++; // 往下
+            }
+        }
+        return false;
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
+//一开始思路是从左上开始,有问题往左变大,往下也变大 ,直接从右上开始,往左变小往下变大更方便
+//1, 暴力,2右上,3二分查找
--- src/leetcode/editor/cn/ID73SetMatrixZeroes .java
+++ src/leetcode/editor/cn/ID73SetMatrixZeroes .java
@@ -11,7 +11,30 @@ class ID73SetMatrixZeroes{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void setZeroes(int[][] matrix) {
-        
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        // 第一次遍历记录0的位置
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // 第二次遍历置0
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
